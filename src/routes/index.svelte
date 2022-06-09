e12a4de284f2fbd6dcf7463dd29d77e8bbee558d
Renato <renato145@hotmail.com>
2022-06-09 22:39:02 +0930

    change round

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,7 +24,7 @@
   const data = getData();
 </script>
 
-<div class="container mx-auto mt-4 rounded-xl bg-slate-50 p-8 ">
+<div class="container mx-auto mt-4 rounded-lg bg-slate-50 p-8 ">
   <h1>Image models</h1>
 
   {#await data}
