a799951bdca96e0ef2ec00ac215b7451d6ec1806
renato <renato.hermoza@pucp.edu.pe>
2022-06-24 05:44:03 -0500

    add gh-pages svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -9,7 +11,13 @@ const config = {
 
   kit: {
     adapter: adapter(),
-    prerender: { default: true }
+    prerender: { default: true },
+    paths: {
+      base: dev ? '' : '/image-models'
+    },
+    // If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+    // For example, instead of '_app', use 'app_', 'internal', etc.
+    appDir: 'internal'
   }
 };
 
