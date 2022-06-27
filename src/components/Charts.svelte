4a2a27ec7386366731a0a27334feb82cd59a17b7
renato <renato.hermoza@pucp.edu.pe>
2022-06-27 04:15:37 -0500

    filter families

--- src/components/Charts.svelte
+++ src/components/Charts.svelte
@@ -6,17 +6,23 @@
   export let data: TData[], chartConfigs;
 
   $: colorScale = scaleOrdinal(schemePaired).domain(data.map(chartConfigs.colorValue));
+  $: allFamilies = colorScale.domain();
+
+  let selectedFamilies = ['vit', 'convnext', 'resnet', 'levit', 'regnetx', 'swin'];
+  const filterFamily = (row: TData) => selectedFamilies.indexOf(row.family) !== -1;
 </script>
 
 <div class="space-y-2">
+  <div>All families: {allFamilies}</div>
+  <div>Selected families: {selectedFamilies}</div>
   <Scatter
-    data={data.filter((row) => row.dataset === 'pets')}
+    data={data.filter((row) => row.dataset === 'pets').filter(filterFamily)}
     title="Oxford IIT-Pet Dataset"
     {...chartConfigs}
     {colorScale}
   />
   <Scatter
-    data={data.filter((row) => row.dataset === 'planet')}
+    data={data.filter((row) => row.dataset === 'planet').filter(filterFamily)}
     title="Kaggle Planet Dataset"
     {...chartConfigs}
     {colorScale}
